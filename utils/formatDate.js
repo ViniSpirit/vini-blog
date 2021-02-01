aea0598a870d64b2a88a42172418e7284f7173fd
Marcos Vinicius <marcosvims@gmail.com>
2021-02-01 16:24:31 -0300

    formatando data

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,6 +1,24 @@
 const formatDate = (date) => {
-  const newDate = new Date(date);
-  return newDate.toUTCString();
+  const meses = [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Abr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Set",
+    "Out",
+    "Nov",
+    "Dez",
+  ];
+  const data = new Date(date);
+  const day = data.getDate() <= 9 ? `0${data.getDate()}` : data.getDate();
+  let dataFormatada =
+    day + "/ " + meses[data.getMonth()] + "/ " + data.getFullYear();
+  console.log(day);
+  return dataFormatada;
 };
 
 export default formatDate;
